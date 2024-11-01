511cb6a37e50f5dc8de9d08aeb1c7ff450ccd2f5
Rohollah <Rohollahhosseini00@gmail.com>
2024-11-01 19:32:22 +0330

    Added Serilog in WebApi Project

---
+++ src/SerilogWebApi/LoggingConfiguration.cs
@@ -0,0 +1,42 @@
+﻿using Serilog;
+using Serilog.Enrichers.Span;
+using Serilog.Exceptions;
+using Serilog.Formatting.Json;
+
+namespace SerilogWebApi
+{
+   /// <summary>
+   /// ما است Serilog این کلاس برای کانفیگ کردن 
+   /// </summary>
+    public class LoggingConfiguration
+    {
+        public static Action<HostBuilderContext, LoggerConfiguration> ConfigurLogger => (context, configuration) =>
+        {
+            #region Enriching Logger Context
+
+            //برای ما مشخص میکنه product در چه مکانی هستیم در حالت توسعه هستیم یا 
+            var env = context.HostingEnvironment;
+
+
+            configuration.Enrich.FromLogContext()
+            .Enrich.WithProperty("ApplicationName", env.ApplicationName)
+            .Enrich.WithProperty("Test","Test")// my information to log
+            .Enrich.WithEnvironmentName()
+            .Enrich.WithSpan()// create ==>TraceId
+            .Enrich.WithMachineName()
+            .Enrich.WithExceptionDetails();
+
+            #endregion
+
+            //logging to Console
+            configuration.WriteTo.Console().MinimumLevel.Error();
+
+
+            //logging to file => .json
+            configuration.WriteTo
+            .Async(sinkConfiguration=>sinkConfiguration.File(new JsonFormatter(),"logs/log.json"))
+            .MinimumLevel.Error();
+
+        };
+    }
+}
---
+++ src/SerilogWebApi/Program.cs
@@ -0,0 +1,59 @@
+﻿using Serilog;
+using SerilogWebApi;
+using System.Diagnostics;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//use serilog
+builder.Host.UseSerilog(LoggingConfiguration.ConfigurLogger);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+    //throw default exception
+
+    //throw new Exception("test Exceptio");
+
+    var forecast = Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+
+    //serilog از  TraceId دریافت 
+    var activityId=Activity.Current?.TraceId.ToHexString()??string.Empty;
+
+    return new {Data=forecast,TracesId=activityId };
+})
+.WithName("GetWeatherForecast")
+.WithOpenApi();
+
+app.Run();
+
+internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
